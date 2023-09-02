f7ea35117487efa68780ab29dd313ea23b934440
bmalvo <boydmalvo@gmail.com>
2023-09-02 14:17:38 +0200

    Navi - > add search button, transition to logo, set responsive

--- script2.js
+++ script2.js
@@ -1,1 +1,10 @@
-//
+// Nav search button
+function goForward() {
+    let search = document.getElementById("form-search");
+
+    if (search.style.marginLeft == "-300px") {
+        search.style.marginLeft = "0px"
+    } else {
+        search.style.marginLeft = "-300px"
+    };
+}
